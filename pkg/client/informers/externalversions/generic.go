f1c739330f62b2a56e813e2be6c1672234e6a385
Szymon Gibała <szymongib@gmail.com>
2020-11-20 11:25:24 +0100

    Prepare types

--- pkg/client/informers/externalversions/generic.go
+++ pkg/client/informers/externalversions/generic.go
@@ -8,7 +8,7 @@ package externalversions
 import (
 	"fmt"
 
-	v1alpha1 "github.com/mattermost/mattermost-operator/apis/mattermost/v1alpha1"
+	v1beta1 "github.com/mattermost/mattermost-operator/apis/mattermost/v1beta1"
 	schema "k8s.io/apimachinery/pkg/runtime/schema"
 	cache "k8s.io/client-go/tools/cache"
 )
@@ -39,9 +39,9 @@ func (f *genericInformer) Lister() cache.GenericLister {
 // TODO extend this to unknown resources with a client pool
 func (f *sharedInformerFactory) ForResource(resource schema.GroupVersionResource) (GenericInformer, error) {
 	switch resource {
-	// Group=mattermost.com, Version=v1alpha1
-	case v1alpha1.SchemeGroupVersion.WithResource("clusterinstallations"):
-		return &genericInformer{resource: resource.GroupResource(), informer: f.Mattermost().V1alpha1().ClusterInstallations().Informer()}, nil
+	// Group=mattermost, Version=v1beta1
+	case v1beta1.SchemeGroupVersion.WithResource("mattermosts"):
+		return &genericInformer{resource: resource.GroupResource(), informer: f.Mattermost().V1beta1().Mattermosts().Informer()}, nil
 
 	}
 
