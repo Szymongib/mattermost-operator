dc3495ee620225d35ccfb623240a5601555795fd
Szymon Gibała <szymongib@gmail.com>
2020-11-24 10:01:52 +0100

    wip 1

---
+++ controllers/mattermost/mattermost/minio.go
@@ -0,0 +1,131 @@
+package mattermost
+
+import (
+	"context"
+	"fmt"
+	mattermostv1beta1 "github.com/mattermost/mattermost-operator/apis/mattermost/v1beta1"
+
+	"github.com/go-logr/logr"
+	corev1 "k8s.io/api/core/v1"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+
+	mattermostMinio "github.com/mattermost/mattermost-operator/pkg/components/minio"
+
+	minioOperator "github.com/minio/minio-operator/pkg/apis/miniocontroller/v1beta1"
+)
+
+// TODO: rename to checkFilestore and move from here
+func (r *MattermostReconciler) checkMinio(mattermost *mattermostv1beta1.Mattermost, reqLogger logr.Logger) error {
+	reqLogger = reqLogger.WithValues("Reconcile", "minio")
+
+	err := r.checkMinioSecret(mattermost, reqLogger)
+	if err != nil {
+		return err
+	}
+
+	if mattermost.Spec.Filestore.IsExternal() {
+		return nil
+	}
+
+	return r.checkMinioInstance(mattermost, reqLogger)
+}
+
+func (r *MattermostReconciler) checkExternalFilestoreSecret(mattermost *mattermostv1beta1.Mattermost, reqLogger logr.Logger) error {
+	secret := &corev1.Secret{}
+	err := r.Client.Get(context.TODO(), types.NamespacedName{Name: mattermost.Spec.Filestore.External.Secret, Namespace: mattermost.Namespace}, secret)
+	if err != nil {
+		reqLogger.Error(err, "failed to check if custom minio secret exists")
+		return err
+	}
+	// Validate custom secret required fields
+	if _, ok := secret.Data["accesskey"]; !ok {
+		return fmt.Errorf("external filestore Secret %s does not have an 'accesskey' value", mattermost.Spec.Filestore.External.Secret)
+	}
+	if _, ok := secret.Data["secretkey"]; !ok {
+		return fmt.Errorf("external filestore Secret %s does not have an 'secretkey' value", mattermost.Spec.Filestore.External.Secret)
+	}
+	return nil
+}
+
+func (r *MattermostReconciler) checkMattermostMinioSecret(mattermost *mattermostv1beta1.Mattermost, reqLogger logr.Logger) error {
+	current := &corev1.Secret{}
+	desired := mattermostMinio.SecretV1Beta(mattermost)
+	err := r.Client.Get(context.TODO(), types.NamespacedName{Name: desired.Name, Namespace: desired.Namespace}, current)
+	switch {
+	case err != nil && kerrors.IsNotFound(err):
+		// Create new secret
+		reqLogger.Info("creating minio secret", "name", desired.Name, "namespace", desired.Namespace)
+		return r.create(mattermost, desired, reqLogger)
+	case err != nil:
+		// Something go wrong badly
+		reqLogger.Error(err, "failed to check if secret exists")
+		return err
+	}
+	// Validate secret required fields, if not exist recreate.
+	if _, ok := current.Data["accesskey"]; !ok {
+		reqLogger.Info("minio secret does not have an 'accesskey' value, overriding", "name", desired.Name)
+		return r.update(current, desired, reqLogger)
+	}
+	if _, ok := current.Data["secretkey"]; !ok {
+		reqLogger.Info("minio secret does not have an 'secretkey' value, overriding", "name", desired.Name)
+		return r.update(current, desired, reqLogger)
+	}
+	// Preserve data fields
+	desired.Data = current.Data
+	return r.update(current, desired, reqLogger)
+}
+
+func (r *MattermostReconciler) checkMinioSecret(mattermost *mattermostv1beta1.Mattermost, reqLogger logr.Logger) error {
+	if mattermost.Spec.Filestore.IsExternal() {
+		return r.checkExternalFilestoreSecret(mattermost, reqLogger)
+	}
+	return r.checkMattermostMinioSecret(mattermost, reqLogger)
+}
+
+func (r *MattermostReconciler) checkMinioInstance(mattermost *mattermostv1beta1.Mattermost, reqLogger logr.Logger) error {
+	desired := mattermostMinio.InstanceV1Beta(mattermost)
+
+	err := r.createMinioInstanceIfNotExists(mattermost, desired, reqLogger)
+	if err != nil {
+		return err
+	}
+
+	current := &minioOperator.MinIOInstance{}
+	err = r.Client.Get(context.TODO(), types.NamespacedName{Name: desired.Name, Namespace: desired.Namespace}, current)
+	if err != nil {
+		return err
+	}
+
+	// Note:
+	// For some reason, our current minio operator seems to remove labels on
+	// the instance resource when we add them. For that reason, trying to
+	// ensure the labels are correct doesn't work.
+	return r.update(current, desired, reqLogger)
+}
+
+func (r *MattermostReconciler) createMinioInstanceIfNotExists(mattermost *mattermostv1beta1.Mattermost, instance *minioOperator.MinIOInstance, reqLogger logr.Logger) error {
+	foundInstance := &minioOperator.MinIOInstance{}
+	err := r.Client.Get(context.TODO(), types.NamespacedName{Name: instance.Name, Namespace: instance.Namespace}, foundInstance)
+	if err != nil && kerrors.IsNotFound(err) {
+		reqLogger.Info("Creating minio instance")
+		return r.create(mattermost, instance, reqLogger)
+	} else if err != nil {
+		reqLogger.Error(err, "Unable to get minio instance")
+		return err
+	}
+
+	return nil
+}
+
+func (r *MattermostReconciler) getMinioService(mattermost *mattermostv1beta1.Mattermost, reqLogger logr.Logger) (string, error) {
+	minioServiceName := fmt.Sprintf("%s-minio-hl-svc", mattermost.Name)
+	minioService := &corev1.Service{}
+	err := r.Client.Get(context.TODO(), types.NamespacedName{Name: minioServiceName, Namespace: mattermost.Namespace}, minioService)
+	if err != nil {
+		return "", err
+	}
+
+	connectionString := fmt.Sprintf("%s.%s:%d", minioService.Name, mattermost.Namespace, minioService.Spec.Ports[0].Port)
+	return connectionString, nil
+}
---
+++ pkg/database/database_mysql_beta.go
@@ -0,0 +1,56 @@
+package database
+
+import (
+	"fmt"
+	"github.com/go-logr/logr"
+	mattermostv1beta1 "github.com/mattermost/mattermost-operator/apis/mattermost/v1beta1"
+	corev1 "k8s.io/api/core/v1"
+)
+
+type MySQLDB struct {
+	secretName string
+	rootPassword string
+	userName     string
+	userPassword string
+	databaseName string
+}
+
+func NewMySQLDB(secret corev1.Secret) (*MySQLDB, error) {
+	rootPassword := string(secret.Data["ROOT_PASSWORD"])
+	if rootPassword == "" {
+		return nil, fmt.Errorf("database root password shouldn't be empty")
+	}
+	userName := string(secret.Data["USER"])
+	if userName == "" {
+		return nil, fmt.Errorf("database username shouldn't be empty")
+	}
+	userPassword := string(secret.Data["PASSWORD"])
+	if userPassword == "" {
+		return nil, fmt.Errorf("database password shouldn't be empty")
+	}
+	databaseName := string(secret.Data["DATABASE"])
+	if databaseName == "" {
+		return nil, fmt.Errorf("database name shouldn't be empty")
+	}
+
+	return &MySQLDB{
+		rootPassword:     rootPassword,
+		userName: userName,
+		userPassword:     userPassword,
+		databaseName:     databaseName,
+	}, nil
+
+}
+
+func (m *MySQLDB) EnvVars(mattermost *mattermostv1beta1.Mattermost) []corev1.EnvVar {
+	panic("implement me")
+}
+
+func (m *MySQLDB) InitContainers() []corev1.Container {
+	panic("implement me")
+}
+
+func (m *MySQLDB) Check(mattermost *mattermostv1beta1.Mattermost, reqLogger logr.Logger) error {
+	panic("implement me")
+}
+
